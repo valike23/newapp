8d4ae08c9bd3cb14b98d6459327c7ce4eba3477a
valike23 <ludike23@gmail.com>
2022-03-06 03:34:57 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'html/about.html'));
 });
 
 module.exports = router;
